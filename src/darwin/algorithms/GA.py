c0f27bf74a451dc9ab6f103751f1f6c147ce66f3
Pavel Polozov <Pavel.Polozov@certara.com>
2022-06-24 15:38:17 +0300

    refactor GA

--- src/darwin/algorithms/GA.py
+++ src/darwin/algorithms/GA.py
@@ -72,41 +72,17 @@ def _add_sharing_penalty(pop, niche_radius, sharing_alpha, niche_penalty):
         else: 
             ind[0].fitness.values = (ind[0].fitness.values[0] + penalty),  # weighted values changes with this
 
-    return
 
-
-def run_ga(model_template: Template) -> ModelRun:
-    """
-    Run the Genetic Algorithm (GA) search, using the DEAP (https://github.com/deap/deap) packages.
-    All the required information is contained in the Template objects, plus the options module
-    The template objects includes the control file template, and all the token groups.
-
-    Called from Darwin.run_search, _run_template
-
-    :param model_template: Template object for the search
-
-    :type model_template: Template
-
-    :return: The single best model from the search
-
-    :rtype: Model
-    """    
-    pop_size = options.population_size
-    downhill_q = options.downhill_q
-    elitist_num = options['elitist_num'] 
-    sharing_alpha = options['sharing_alpha']
-    niche_penalty = options['niche_penalty']   
-    num_bits = int(np.sum(model_template.gene_length))
-
+def _create_toolbox(num_bits) -> deap.base.Toolbox:
     creator.create("FitnessMin", deap.base.Fitness, weights=(-1.0,))
     creator.create("Individual", list, fitness=creator.FitnessMin)
 
     toolbox = deap.base.Toolbox()
-    # Attribute generator 
+    # Attribute generator
     #                      define 'attr_bool' to be an attribute ('gene')
     #                      which corresponds to integers sampled uniformly
     #                      from the range [0,1] (i.e. 0 or 1 with equal
-    #                      probability) 
+    #                      probability)
     random.seed(options['random_seed'])
     toolbox.register("attr_bool", random.randint, 0, 1)
 
@@ -119,7 +95,7 @@ def run_ga(model_template: Template) -> ModelRun:
     # define the population to be a list of individuals
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-    # the goal ('fitness') function to be maximized  
+    # the goal ('fitness') function to be maximized
 
     # register the crossover operator
     if options['crossoverOperator'] == "cxOnePoint":
@@ -137,46 +113,25 @@ def run_ga(model_template: Template) -> ModelRun:
     # drawn randomly from the current generation.
     if options['selection'] == "tournament":
         toolbox.register("select", tools.selTournament, tournsize=options['selection_size'])
- 
-    # create an initial population of pop_size individuals (where
-    # each individual is a list of bits [0|1])
-    pop_full_bits = toolbox.population(n=pop_size)
-    best_for_elitism = toolbox.population(n=elitist_num)   
-    crossover_probability = options['crossoverRate']
-    mutation_probability = options['mutationRate']
 
-    first_gen = Population.from_codes(model_template, 0, pop_full_bits, ModelCode.from_full_binary)
+    return toolbox
 
-    first_gen.run_all()
 
-    log.message(f"Best overall fitness = {GlobalVars.BestRun.result.fitness:4f},"
-                f" iteration 0, model {GlobalVars.BestRun.model_num}")
+class DeapToolbox:
+    def __init__(self, template: Template):
+        self.toolbox = _create_toolbox(int(np.sum(template.gene_length)))
 
-    fitnesses = [r.result.fitness for r in first_gen.runs]
+    def new_population(self, pop_size):
+        return self.toolbox.population(n=pop_size)
 
-    for run, pop in zip(first_gen.runs, pop_full_bits):
-        pop.fitness.values = (run.result.fitness,)
+    def get_offspring(self, pop_full_bits):
+        toolbox = self.toolbox
 
-    best_index = get_n_best_index(elitist_num, fitnesses)
+        sharing_alpha = options['sharing_alpha']
+        niche_penalty = options['niche_penalty']
 
-    for i in range(elitist_num):  # best_index:
-        best_for_elitism[i] = deepcopy(pop_full_bits[best_index[i]])
-
-    # Variable keeping track of the number of generations
-    generation = 0
-
-    # Begin evolution
-
-    generations_no_change = 0
-    overall_best_fitness = options.crash_value
-    num_generations = options['num_generations']
-
-    population = Population(model_template, generation)
-
-    while generation < num_generations:
-        # A new generation
-        generation += 1
-        log.message("-- Starting Generation %i --" % generation)
+        crossover_probability = options['crossoverRate']
+        mutation_probability = options['mutationRate']
 
         # will change the values in pop, but not in fitnesses, need to run downhill from fitness values, not from pop
         # so fitnesses in pop are only used for selection in GA
@@ -186,7 +141,7 @@ def run_ga(model_template: Template) -> ModelRun:
         # Select the next generation individuals
         offspring = toolbox.select(pop_full_bits, len(pop_full_bits))
         # Clone the selected individuals, otherwise will be linked to original, by reference
-        offspring = list(map(toolbox.clone, offspring))
+        offspring = [toolbox.clone(x) for x in offspring]
 
         # Apply crossover and mutation on the offspring
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
@@ -205,77 +160,133 @@ def run_ga(model_template: Template) -> ModelRun:
                 toolbox.mutate(mutant)
                 del mutant.fitness.values
 
-        # Evaluate the individuals with an invalid fitness
-        # will run entire population, but, at some point, check a database to see if already run
-        pop_full_bits = offspring
+        return offspring
 
-        # add hof back in at first  positions, maybe should be random???
-        # looks like we need to do this manually when we add in niches, can't use hof.,
-        # replace worst, based on original fitness (without niche penalty)
-        worst_run_indices = get_n_worst_index(elitist_num, fitnesses)
 
-        # put elitist back in place of worst
-        for i in range(elitist_num):
-            pop_full_bits[worst_run_indices[i]] = copy(best_for_elitism[i])  # hof.items need the fitness as well?
+def _model_run_to_deap_ind(run: ModelRun):
+    res = creator.Individual(run.model.model_code.FullBinCode)
+    res.fitness.setValues((run.result.fitness,))
 
-        population = Population.from_codes(model_template, generation, pop_full_bits, ModelCode.from_full_binary)
+    return res
+
+
+class _GenerationRunner:
+    def __init__(self, template: Template, elitist_num):
+        self.generation = -1
+        self.fitnesses = []
+        self.template = template
+        self.elitist_num = elitist_num
+
+    def run_generation(self, pop_full_bits, best_for_elitism):
+        self.generation += 1
+
+        log.message("-- Starting Generation %i --" % self.generation)
+
+        elitist_num = self.elitist_num
+
+        if self.fitnesses:
+            # add hof back in at first  positions, maybe should be random???
+            # looks like we need to do this manually when we add in niches, can't use hof.,
+            # replace worst, based on original fitness (without niche penalty)
+            worst_run_indices = get_n_worst_index(elitist_num, self.fitnesses)
+
+            # put elitist back in place of worst
+            for i in range(elitist_num):
+                pop_full_bits[worst_run_indices[i]] = copy(best_for_elitism[i])  # hof.items need the fitness as well?
+
+        population = Population.from_codes(self.template, self.generation, pop_full_bits, ModelCode.from_full_binary)
 
         population.run_all()
 
         if not keep_going():
-            break
+            return population, False
 
         runs = population.runs
 
-        best_run = GlobalVars.BestRun
-
-        log.message(f"Best overall fitness = {best_run.result.fitness:4f},"
-                    f" iteration {best_run.generation}, model {best_run.model_num}")
-
-        fitnesses = [None]*len(runs)
-
-        for run, pop, fit in zip(runs, pop_full_bits, range(len(runs))):
+        for run, pop in zip(runs, pop_full_bits):
             pop.fitness.values = (run.result.fitness,)
-            fitnesses[fit] = run.result.fitness
 
-        best_index = get_n_best_index(elitist_num, fitnesses)
+        self.fitnesses = [r.result.fitness for r in runs]
 
-        for i in range(elitist_num):  # best_index:
+        best_index = get_n_best_index(elitist_num, self.fitnesses)
+
+        for i in range(elitist_num):
             best_for_elitism[i] = deepcopy(pop_full_bits[best_index[i]])
 
-        if generation % downhill_q == 0 and keep_going():
+        return population, True
+
+
+def run_ga(model_template: Template) -> ModelRun:
+    """
+    Run the Genetic Algorithm (GA) search, using the DEAP (https://github.com/deap/deap) packages.
+    All the required information is contained in the Template objects, plus the options module
+    The template objects includes the control file template, and all the token groups.
+
+    Called from Darwin.run_search, _run_template
+
+    :param model_template: Template object for the search
+
+    :type model_template: Template
+
+    :return: The single best model from the search
+
+    :rtype: Model
+    """    
+    downhill_q = options.downhill_q
+    elitist_num = options['elitist_num']
+
+    pop_size = options.population_size
+
+    toolbox = DeapToolbox(model_template)
+
+    # create an initial population of pop_size individuals (where
+    # each individual is a list of bits [0|1])
+    pop_full_bits = toolbox.new_population(pop_size)
+    best_for_elitism = toolbox.new_population(elitist_num)
+
+    runner = _GenerationRunner(model_template, elitist_num)
+
+    population, cont = runner.run_generation(pop_full_bits, best_for_elitism)
+
+    if not cont:
+        return GlobalVars.BestRun
+
+    # Begin evolution
+
+    generations_no_change = 0
+    overall_best_fitness = options.crash_value
+    num_generations = options['num_generations']
+
+    while runner.generation < num_generations:
+        pop_full_bits = toolbox.get_offspring(pop_full_bits)
+
+        population, cont = runner.run_generation(pop_full_bits, best_for_elitism)
+
+        if not cont:
+            break
+
+        if runner.generation % downhill_q == 0:
             # pop will have the fitnesses without the niche penalty here
             # add local exhaustive search here??
             # temp_fitnesses = copy(fitnesses)
             # downhill with NumNiches best models
-            log.message(f"Starting downhill generation = {generation}  at {time.asctime()}")
+            log.message(f"Starting downhill generation = {runner.generation}  at {time.asctime()}")
 
             best_runs = population.get_best_runs(options.num_niches)
 
             log.message(f"current best model(s) =")
 
             for run in best_runs:
-                log.message(f"generation {generation}, ind {run.model_num}, fitness = {run.result.fitness}")
+                log.message(f"generation {runner.generation}, ind {run.model_num}, fitness = {run.result.fitness}")
 
             run_downhill(model_template, population)
 
-            best_run = GlobalVars.BestRun
-
-            log.message(f"Best overall fitness = {best_run.result.fitness:4f},"
-                        f" iteration {best_run.generation}, model {best_run.model_num}")
-
             best_runs = population.get_best_runs(elitist_num)
 
-            log.message(f"Done with downhill step, {generation}. best fitness = {best_runs[0].result.fitness}")
+            log.message(f"Done with downhill step, {runner.generation}. best fitness = {best_runs[0].result.fitness}")
 
             # redo best_for_elitism, after downhill
-
-            num_bits = len(best_runs[-1].model.model_code.FullBinCode)
-
-            for i in range(elitist_num):  # best_index:
-                # this is GA, so need full binary code
-                best_for_elitism[i][0:num_bits] = best_runs[i].model.model_code.FullBinCode
-                best_for_elitism[i].fitness.values = (best_runs[i].result.fitness,)
+            best_for_elitism = [_model_run_to_deap_ind(run) for run in best_runs]
 
         best_run = population.get_best_run()
 
@@ -284,8 +295,13 @@ def run_ga(model_template: Template) -> ModelRun:
         log.message(f"Current generation best genome = {best_run.model.model_code.FullBinCode},"
                     f" best fitness = {best_fitness:.4f}")
 
+        best_run_overall = GlobalVars.BestRun
+
+        log.message(f"Best overall fitness = {best_run_overall.result.fitness:4f},"
+                    f" iteration {best_run_overall.generation}, model {best_run_overall.model_num}")
+
         if best_fitness < overall_best_fitness:
-            log.message(f"Better fitness found, generation = {generation}, new best fitness = {best_fitness:.4f}")
+            log.message(f"Better fitness found, generation = {runner.generation}, new best fitness = {best_fitness:.4f}")
             overall_best_fitness = best_fitness
             generations_no_change = 0
         else:
@@ -304,7 +320,7 @@ def run_ga(model_template: Template) -> ModelRun:
 
         best = population.get_best_run()
 
-        log.message(f"Done with final downhill step, {generation}. best fitness = {best.result.fitness}")
+        log.message(f"Done with final downhill step. best fitness = {best.result.fitness}")
 
     log.message(f"-- End of Optimization at {time.asctime()} --")
 

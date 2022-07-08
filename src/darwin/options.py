6cc8020414c21c9d93f409e4c9735fa664554981
Pavel Polozov <Pavel.Polozov@certara.com>
2022-07-08 17:02:20 +0300

    NM_priority_class -> model_run_priority_class NM_timeout_sec -> model_run_timeout R_timeout_sec -> R_timeout

--- src/darwin/options.py
+++ src/darwin/options.py
@@ -150,8 +150,8 @@ class Options:
         log.message(f"NMFE found at {self.nmfe_path}")
 
         self.model_run_priority = _get_priority_class(opts)
-        self.model_run_timeout = int(opts.get('NM_timeout_sec', 1200))
-        self.r_timeout = int(opts.get('R_timeout_sec', 90))
+        self.model_run_timeout = int(opts.get('model_run_timeout', 1200))
+        self.r_timeout = int(opts.get('R_timeout', 90))
 
         self.search_omega_bands = opts.get('search_omega_bands', False)
         self.max_omega_band_width = opts.get('max_omega_band_width', 0)

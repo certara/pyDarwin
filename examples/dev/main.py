2b73a58192082bcd5cf4987199ab6b8b017eda66
Pavel Polozov <Pavel.Polozov@certara.com>
2022-08-09 00:58:22 +0300

    main.py is not a part of src

---
+++ examples/dev/main.py
@@ -0,0 +1,36 @@
+""""
+Sponsor:
+Program:
+Programmer’s Name: Certara
+Date:31July2022
+ 
+Platform: Windows
+Environment:  
+Modified By: Certara
+    Date: 31 July 2022 
+""" 
+from darwin.run_search import run_search
+
+if __name__ == '__main__':  
+    # final = run_search("..\\user\\Example1\\template.txt",
+    #                    "..\\user\\Example1\\tokens.json",
+    #                    "..\\user\\Example1\\options.json")
+
+    # final = run_search("..\\user\\example2\\template.txt",
+    #                    "..\\user\\Example2\\tokens.json",
+    #                    "..\\user\\Example2\\options.json")
+
+    # final = run_search("..\\user\\Example3\\template.txt",
+    #                    "..\\user\\Example3\\tokens.json",
+    #                    "..\\user\\Example3\\options.json")
+
+    # final = run_search("..\\user\\Example4\\template.txt",
+    #                    "..\\user\\Example4\\tokens.json",
+    #                    "..\\user\\Example4\\options.json")
+
+    final = run_search("..\\user\\Example5\\template.txt",
+                       "..\\user\\Example5\\tokens.json",
+                       "..\\user\\Example5\\options.json")
+    # final = run_search("..\\user\\Example6\\template.txt",
+    #                     "..\\user\\Example6\\tokens.json",
+    #                     "..\\user\\Example6\\options.json")

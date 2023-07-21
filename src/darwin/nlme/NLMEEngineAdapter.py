6f8f8b0b22befab1b0af10c64a22db78ef8d6535
Alex Mazur <alex.mazur@certara.com>
2023-07-21 16:09:07 +0300

    [DRWN-770] [pyDarwin][NLME] Omega search
    fix number of omegas

--- src/darwin/nlme/NLMEEngineAdapter.py
+++ src/darwin/nlme/NLMEEngineAdapter.py
@@ -360,7 +360,7 @@ class NLMEEngineAdapter(ModelEngineAdapter):
         (si_descr, si_fix) = extract_lhs(error)
 
         theta_num = len(th_descr)
-        omega_num = len(om_descr)
+        omega_num = len(om_block)
         sigma_num = len(si_descr)
 
         estimated_theta = theta_num
@@ -371,7 +371,7 @@ class NLMEEngineAdapter(ModelEngineAdapter):
             estimated_theta -= (th_fix[i] or th_low[i] == th_up[i] and th_low[i] != '')
 
         for i in range(omega_num):
-            estimated_omega -= (om_fix[i])
+            estimated_omega -= (om_block[i])
 
         for i in range(sigma_num):
             estimated_sigma -= (si_fix[i])
